93a709b2fa4f62ab5f28f53fc897f986ab28319b
Azjinsol <96511423+Azjinsol@users.noreply.github.com>
2023-05-18 01:36:55 -0700

    Update Challenge_2.sql

--- Challenge_2.sql
+++ Challenge_2.sql
@@ -1,9 +1,41 @@
 /*
 Created By: Azjinsol 
 Create Date: 5/17/2023
-Description: List of customers' transaction and employees and the average of it
+Description: List of customers' transaction and the average of it
 */
 
+/*
+CHALLENGE 2
+More targeted questions that query tables containing data about customers and employees
+1. Get a list of customers who made purchases between 2011 and 2012.
+2. Get a list of customers, sales reps, and total transaction amounts for each customer 
+between 2011 and 2012.
+3. How many transactions are above the average transaction amount during the same 
+time period?
+4. What is the average transaction amount for each year that WSDA Music has been 
+in business?
+*/
+
+-- 1.)
+/*
+SELECT
+	c.FirstName AS [CustomerFN],
+	c.LastName AS [CustomerLN],
+	i.total
+FROM
+	Invoice i 
+INNER JOIN 
+	Customer c
+ON 
+	i.CustomerId = c.CustomerId
+WHERE 
+	InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
+ORDER BY 
+	i.total DESC 
+*/
+	
+-- 2.)
+/*
 SELECT
 	c.FirstName AS [CustomerFN],
 	c.LastName AS [CustomerLN],
@@ -24,4 +56,27 @@ WHERE
 	InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
 ORDER BY 
 	i.total DESC 
-	
+*/
+
+--3.)
+/*
+SELECT 
+	COUNT(total) AS [Num of Transactions Above Avg]
+FROM 
+	Invoice
+WHERE 
+	total > 
+		(SELECT	
+			ROUND(AVG(total), 2) AS [Avg Transaction Amount]
+		FROM 
+			Invoice
+		WHERE 
+			InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31')
+AND InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2012-12-31'
+*/
+
+--4.)
+SELECT
+
+FROM 
+	Invoice
